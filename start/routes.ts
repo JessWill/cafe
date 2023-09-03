6bc0b59aef9a108bcf8d640ad92d516cc34020b8
Jess Williams <jessica_williams0@protonmail.com>
2023-09-03 16:47:17 -0400

    routes and front end stuff

--- start/routes.ts
+++ start/routes.ts
@@ -21,5 +21,10 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+  return view.render('home')
 })
+
+Route.get('/submission', async ({ view }) => {
+  return view.render('submission')
+})
+
