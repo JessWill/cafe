b22d0f8d093e5244e9389783af191dcb2a75674e
JessWill <jessica_williams0@protonmail.com>
2023-10-21 18:56:18 -0400

    remove the dumb view counter from assignment

--- database/seeders/ViewCounter.ts
+++
@@ -1,7 +0,0 @@
-import Database from '@ioc:Adonis/Lucid/Database'
-
-export default class ViewCounterSeeder {
-  public async run() {
-    await Database.table('viewcounters').insert({ count: 0 })
-  }
-}
--- start/kernel.ts
+++ start/kernel.ts
@@ -20,10 +20,7 @@ import Server from '@ioc:Adonis/Core/Server'
 | are defined for every HTTP requests.
 |
 */
-Server.middleware.register([
-    () => import('@ioc:Adonis/Core/BodyParser'),
-    'App/Middleware/ViewCounter'
-  ])
+
   
 
 /*
